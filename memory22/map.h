69ace7ff2e18fcf5b32c2f5c9843069901f19030
Bruna Ricardo Coimbra (ricardob) <ricardob@coventry.ac.uk>
2019-03-22 20:18:47 +0000

    functioning map

--- memory22/map.h
+++ memory22/map.h
@@ -2,26 +2,25 @@
 #define map_hpp
 #include <stdio.h>
 #include "game.h"
-#include <string>
 
-//Class that refers to all of the environment and textures of the game
 class map{
     
 public:
     map(SDL_Renderer* ren);
     ~map();
     
-    std::vector<std::string> createArray(std::string textFile);
-    void drawMap(SDL_Renderer* ren, std::string textFile);
+    void loadMap(int array[32][32]);
+    void drawMap(SDL_Renderer* ren);
     
 private:
     
-    SDL_Rect src_rect, dest_rect; //rectangle that will render on screen
+    SDL_Rect src_rect, dest_rect;
     
-    //textures in the environment
-    SDL_Texture* tree;
+    SDL_Texture* dirt;
     SDL_Texture* grass;
     SDL_Texture* water;
+    
+	int Map[32][32];
     
 };
 #endif /* map_hpp */
